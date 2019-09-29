5a98a0b1c4943070754d13e7584d2fb4dadf9aa5
Adhitya Mohan <me@adhityamohan.in>
2019-09-29 22:43:50 +0530

    Revert "msm8998-common: DeviceParts: migrate to androidx"
    This reverts commit 191ab1ccca897a7314baacaec7827f760324d95a.

--- DeviceParts/Android.mk
+++ DeviceParts/Android.mk
@@ -4,8 +4,12 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_STATIC_ANDROID_LIBRARIES := \
-    androidx.appcompat_appcompat
-
+    android-support-v14-preference \
+    android-support-v13 \
+    android-support-v7-appcompat \
+    android-support-v7-preference \
+    android-support-v7-recyclerview \
+    android-support-v4
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := DeviceParts
 LOCAL_CERTIFICATE := platform
